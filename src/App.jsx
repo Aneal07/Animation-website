500513c54d08d7c4f350942b6df910b831ca5936
Aneal07 <anilmaharjan07@outlook.com>
2025-02-21 12:58:55 +0000

    added pre-fix themes & colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 const App = () => {
   return (
     <main>
-     <h1 className='text-5xl text-orange-500 font-bold'>Application</h1>
+     <h1 className='text-5xl text-voilet-300 text-orange-500 font-bold'>Application</h1>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sanf-serif"],
+        general: ["general", "sanf-serif"],
+        "circular-web": ["circular-web", "sanf-serif"],
+        "robert-meduim": ["robert-meduim", "sanf-serif"],
+        "robert-regular": ["robert-regular", "sanf-serif"],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#DFDFF2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        voilet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8E983f",
+          300: "#EDFF66",
+        },
+      },
+    },
   },
   plugins: [],
 };
