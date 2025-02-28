65e4e1681343298b8d5a95c65a185a8f9f9a4766
Aneal07 <anilmaharjan07@outlook.com>
2025-02-28 14:14:57 +0000

    added about section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,8 @@
+export const AnimatedTitle = () => {
+  return (
+    <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem'>
+      Disc<b>o</b>ver the world's <br />
+      <b>l</b>argest shared <b>a</b>dventure
+    </div>
+  )
+}
